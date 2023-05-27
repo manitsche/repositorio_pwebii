d5cd5be38b1e0c806c186bc3bcbf3078d4c3b048
Marco Antonio Nitsche <105062935+manitsche@users.noreply.github.com>
2023-05-26 22:17:09 -0300

    Delete Aula 09052023 - Atividade avaliativa 1 [2° bimestre]/Typescript directory

--- Aula 09052023 - Atividade avaliativa 1 [2° bimestre]/Typescript/src/app/useCases/alunos/listAlunos.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from "express";
-import { Aluno } from "../../models/Aluno";
-
-export async function listAlunos(req: Request, res: Response) {
-    const alunos = await Aluno.find();
-
-    res.json(alunos);
-}
